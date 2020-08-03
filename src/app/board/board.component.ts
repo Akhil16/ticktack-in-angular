4fa359e1806c4e27167354c57d4281a9a7f9a7bd
Akhilendu Chaturvedi(IN1002) <m1051002@mindtree.com>
2020-08-03 13:07:39 +0530

    for of used

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -49,14 +49,9 @@ export class BoardComponent implements OnInit {
       [0, 4, 8],
       [2, 4, 6]
     ];
-    // lines.forEach(line => {
-    //   const [a, b, c] = line;
-    //   if (this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
-    //     return this.squares[a];
-    //   }
-    // });
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
+
+    for (const i of lines) {
+      const [a, b, c] = i;
       if (
         this.squares[a] &&
         this.squares[a] === this.squares[b] &&
